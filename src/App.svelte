1b2724f01e2d6883d6d00866e642ef84134cd312
Justice-Goto-3939 <hinata.hakuba001@gmail.com>
2024-01-22 17:24:05 +0900

    第2章しゅうりょう

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,16 @@
-<script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-</script>
+<main class="flex flex-col h-dvh">
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div class="bg-red-200 flex justify-around text-xl font-bold p-3">
+    <div>スコア:0</div>
+    <dev>連続正解数:0</dev>
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <div class="bg-green-200 text-center text-4xl py-4">鮪</div>
+  
+  <div class="bg-blue-200 flex flex-col justify-around flex-grow items-center">
+    <button class="bg-blue-500 text-lg font-bold text-blue-50 border-blue-800 border-2 rounded-xl py-4 w-4/5">まぐろ</button>
+    <button class="bg-blue-500 text-lg font-bold text-blue-50 border-blue-800 border-2 rounded-xl py-4 w-4/5">ぶり</button>
+    <button class="bg-blue-500 text-lg font-bold text-blue-50 border-blue-800 border-2 rounded-xl py-4 w-4/5">くじら</button>
+  </div>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
